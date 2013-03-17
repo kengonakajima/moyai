17259dcff2f376a3beaba5757909f923bdc0562f
Kengo Nakajima <kengo.nakajima@gmail.com>
2013-03-18 06:38:03 +0900

    getmtime header

--- cumino.h
+++ cumino.h
@@ -124,6 +124,7 @@ inline int sign(float f){
 
 bool writeFile( const char *path, const char *data, size_t sz );
 bool readFile( const char *path, char *data, size_t *sz );
+int getModifiedTime( const char *path, time_t *out );
 
 void dump(const char*s, size_t l);
 #endif
