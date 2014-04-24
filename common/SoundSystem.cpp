106974dd8f9885b8150cf06eba4fe752f9f0eaa1
Kengo Nakajima <kengo.nakajima@gmail.com>
2014-04-24 12:37:44 +0900

    initialization

--- common/SoundSystem.cpp
+++ common/SoundSystem.cpp
@@ -1,7 +1,7 @@
 #include "SoundSystem.h"
 #include "Sound.h"
 
-SoundSystem::SoundSystem()  : sys(0) {
+SoundSystem::SoundSystem()  : id_gen(1), sys(0) {
 	FMOD_RESULT r;
 	r = FMOD_System_Create(&sys);
 	FMOD_ERRCHECK(r);
