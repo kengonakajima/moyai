92c4c30a1a543a2e5685a8779510da1d5350fb78
Kengo Nakajima <kengo.nakajima@gmail.com>
2016-01-14 17:10:16 +0900

    demo2d runs on windows

--- opengl/Dependencies_OGL.h
+++ opengl/Dependencies_OGL.h
@@ -2,5 +2,5 @@
 
 #pragma comment(lib, "glew32sd.lib")
 #pragma comment(lib, "glut32.lib")
-#pragma comment(lib, "GLFW.lib")
+#pragma comment(lib, "glfw3.lib")
 #pragma comment(lib, "freetype-gl.lib")
