8454f9444ed81c38bf3af92d83dccf59d886cbb7
Kengo Nakajima <kengo.nakajima@gmail.com>
2016-03-25 14:26:59 +0900

    toggle flags

--- Mouse.h
+++ Mouse.h
@@ -8,6 +8,7 @@ public:
     Vec2 cursor_pos;
     static const int BUTTON_MAX = 8;
     int buttons[BUTTON_MAX];
+    bool toggled[BUTTON_MAX];
     int mod_shift;
     int mod_ctrl;
     int mod_alt;
@@ -21,6 +22,9 @@ public:
     }
     void updateButton( unsigned int button, int action, int modshift, int modctrl, int modalt ) {
         validateButtonIndex(button);
+        if(action) {
+            if( !buttons[button] ) toggled[button] = true;
+        } 
         buttons[button] = action;
         mod_shift = modshift;
         mod_ctrl = modctrl;
@@ -29,6 +33,14 @@ public:
     int getButton( unsigned int button ) {
         validateButtonIndex(button);
         return buttons[button];
+    }
+    int getToggled( unsigned int button ) {
+        validateButtonIndex(button);
+        return toggled[button];
+    }
+    void clearToggled( unsigned int button ) {
+        validateButtonIndex(button);
+        toggled[button] = false;        
     }
     void updateCursorPosition( float x, float y ) {
         cursor_pos.x = x;
