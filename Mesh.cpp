7745957de5c46dabe8d840dd100da36723d3ea0d
Kengo Nakajima <kengo.nakajima@gmail.com>
2016-07-05 14:07:25 +0900

    dump limits

--- Mesh.cpp
+++ Mesh.cpp
@@ -4,6 +4,6 @@
 
 void Mesh::dump() {
 	print("mesh: primtype:%d transparent:%d", prim_type, transparent );
-	if(vb) vb->dump(); else print("no-vb" );
-	if(ib) ib->dump(); else print("no-ib" );
+	if(vb) vb->dump(10); else print("no-vb" );
+	if(ib) ib->dump(10); else print("no-ib" );
 }
