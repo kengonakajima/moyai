300208ac4d13665db79e310bef2799ee1d7044be
Kengo Nakajima <kengo.nakajima@gmail.com>
2016-10-08 16:14:56 +0900

    dir4

--- Pad.h
+++ Pad.h
@@ -23,6 +23,9 @@ public:
 		v->x = dx;
 		v->y = dy;
 	}
+    DIR4 getDir() {
+        if(up) return DIR4_UP; else if(down) return DIR4_DOWN; else if(right) return DIR4_RIGHT; else if(left) return DIR4_LEFT; else return DIR4_NONE;
+    }
     void setUpKey( bool u) { up = u; }
     void setDownKey( bool d) { down = d; }
     void setLeftKey( bool l ) { left = l; }
