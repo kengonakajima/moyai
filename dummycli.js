6c7309067c3422a9a729c768e13bf247a5e2620c
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-05-28 10:17:14 +0900

    multiply

--- dummycli.js
+++ dummycli.js
@@ -4,33 +4,57 @@
 
 var net = require('net');
 
-var cnt=0;
+
 var total_recv_bytes=0;
 var total_recv_count=0;
 
-var client = new net.Socket();
-client.connect(22222, '127.0.0.1', function() {
-	console.log('Connected. cnt:',cnt);
-});
+var num_cli = parseInt( process.argv[2] );
+var host = process.argv[3];
 
-client.on('data', function(data) {
-    total_recv_count ++;
-    total_recv_bytes += data.length;	
-});
+if( num_cli == NaN || !host ) {
+    console.log("Usage: node dummycli.js NUM_CLI SERVER_ADDR");
+    process.exit(1);
+}
+console.log("num_cli:", num_cli, "host:", host );
 
-client.on('close', function() {
-	console.log('Connection closed');
-});
+function startNewClient(host) {
+    var client = new net.Socket();
+    client.connect(22222, host, function() {
+	    console.log("Connected");
+    });
+
+    client.on('data', function(data) {
+        total_recv_count ++;
+        total_recv_bytes += data.length;	
+    });
+
+    client.on('close', function() {
+	    console.log('Connection closed');
+    });
+}
+
+
+
+
 
 var last_tot_bytes=0;
 var last_tot_cnt=0;
+
+var cli_started=0;
 
 setInterval(function() {
     var bytes_per_sec = total_recv_bytes - last_tot_bytes;
     var count_per_sec = total_recv_count - last_tot_cnt;
     
-    console.log("total recv bytes:", total_recv_bytes, " count:", total_recv_count,  "bytes/sec:", bytes_per_sec, "count/sec:", count_per_sec );
+    console.log("num_cli:", num_cli, "cli_started:", cli_started, "total recv bytes:", total_recv_bytes, " count:", total_recv_count,  "bytes/sec:", bytes_per_sec, "count/sec:", count_per_sec );
     last_tot_bytes = total_recv_bytes;
     last_tot_cnt = total_recv_count;
-    
 }, 1000 );
+
+setInterval(function() {
+    if( cli_started < num_cli) {
+        startNewClient(host);
+        cli_started++;
+    }
+},100);
+
