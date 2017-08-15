0cb5cf3732ce0003aa116d0cc5330467d71c39b6
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-08-16 08:12:32 +0900

    light

--- demo3d.cpp
+++ demo3d.cpp
@@ -24,12 +24,14 @@ Texture *g_sol_tex;
 Mesh *g_colmesh;
 Mesh *g_texmesh;
 Mesh *g_texcolmesh;
+Mesh *g_texnormmesh;
 
 
-
+Light *g_light;
+Material *g_material;
 
 Prop3D *g_prop_col;
-Prop3D *g_prop_tex;
+Prop3D *g_prop_texnorm;
 Prop3D *g_prop_texcol;
 Prop3D *g_prop_with_children;
 Prop3D *g_children[3];
@@ -56,21 +58,20 @@ void updateGame() {
     }
 
     // move camera
-    Vec3 campos = g_main_camera->loc;
-    g_main_camera->setLoc( campos + Vec3(dt,0,0));    
-
+    //    Vec3 campos = g_main_camera->loc;
+    //    g_main_camera->setLoc( campos + Vec3(dt,0,0));    
+    
+    // props
     if( g_prop_col ){
         g_prop_col->loc.x += dt/10;
         g_prop_col->rot.z += dt*100;
         g_prop_col->rot.y += dt*100;        
     }
-
-    if( g_prop_tex ){
-        g_prop_tex->loc.x -= dt/10;
-        g_prop_tex->rot.x -= dt*50;
-        g_prop_tex->rot.z -= dt*30;
+    if( g_prop_texnorm ){
+        g_prop_texnorm->loc.x -= dt/10;
+        g_prop_texnorm->rot.x -= dt*50;
+        g_prop_texnorm->rot.z -= dt*30;
     }
-
     if( g_prop_texcol ){
         g_prop_texcol->loc.y += dt/10;
         g_prop_texcol->rot.x -= dt*100;
@@ -209,11 +210,9 @@ void setupCube() {
     texcolvb->setColor( 5, Color(1,0,1,0.5) );    
     texcolvb->setColor( 6, Color(1,1,1,0.5) );    
     texcolvb->setColor( 7, Color(1,1,1,0.5) );        
-    
 
-    
-    // for cube (counter clockwise)
-    IndexBufferType cube_indexes[36] = {
+    // for cube without normals (counter clockwise)
+    IndexBufferType cube_indexes_wo_normals[36] = {
         // bottom
         0,3,1, // ADB 
         3,2,1, // DCB
@@ -239,24 +238,132 @@ void setupCube() {
         7,6,2, // HGC
     };
 
-    IndexBuffer *cube_ib = new IndexBuffer();
-    cube_ib->set(cube_indexes,36);
+    IndexBuffer *cube_ib_wo_normals = new IndexBuffer();
+    cube_ib_wo_normals->set(cube_indexes_wo_normals,36);
+    
+
+    // for cube with normals (counter clockwise)
+    IndexBufferType cube_indexes_with_normals[36] = {
+        // bottom ABCD
+        0,3,1, // ADB 
+        3,2,1, // DCB
+        
+        // top EFGH 
+        7,5,6, // HFG
+        4,5,7, // EFH
+
+        // left ADHE : 8,9,10,11
+        11,9,8, // EDA
+        11,10,9, // EHD
+
+        // right BCGH : 12,13,14,15
+        15,12,13, // FBC
+        15,13,14, // FCG
+
+        // front ABFE : 16,17,18,19
+        19,16,17, // EAB
+        19,17,18, // EBF
+
+        // rear DCGH : 20,21,22,23
+        23,21,20, // HCD
+        23,22,21, // HGC
+    };
+
+    IndexBuffer *cube_ib_with_normals = new IndexBuffer();
+    cube_ib_with_normals->set(cube_indexes_with_normals,36);
+    
+    // cube with tex and normal
+    VertexFormat *texnormvf = new VertexFormat();
+    texnormvf->declareCoordVec3();
+    texnormvf->declareUV();
+    texnormvf->declareNormal();    
+
+
+    VertexBuffer *texnormvb = new VertexBuffer();
+    texnormvb->setFormat(texnormvf);
+    texnormvb->reserve(6*4); // need normals for A~H for flat shading
+
+    Vec3 _a(-d,-d,d);  // A red
+    Vec3 _b(d,-d,d); // B blue
+    Vec3 _c(d,-d,-d); // C yellow
+    Vec3 _d(-d,-d,-d); // D green
+    Vec3 _e(-d,d,d); // E white
+    Vec3 _f(d,d,d); // F purple
+    Vec3 _g(d,d,-d); // G white
+    Vec3 _h(-d,d,-d); // H white
+
+    // ABCD
+    texnormvb->setCoord( 0, _a); texnormvb->setCoord( 1, _b); texnormvb->setCoord( 2, _c); texnormvb->setCoord( 3, _d); 
+    // EFGH
+    texnormvb->setCoord( 4, _e); texnormvb->setCoord( 5, _f); texnormvb->setCoord( 6, _g); texnormvb->setCoord( 7, _h);
+    // ADHE
+    texnormvb->setCoord( 8, _a); texnormvb->setCoord( 9, _d); texnormvb->setCoord(10, _h); texnormvb->setCoord(11, _e);
+    // BCGF
+    texnormvb->setCoord(12, _b); texnormvb->setCoord(13, _c); texnormvb->setCoord(14, _g); texnormvb->setCoord(15, _f);
+    // ABFE
+    texnormvb->setCoord(16, _a); texnormvb->setCoord(17, _b); texnormvb->setCoord(18, _f); texnormvb->setCoord(19, _e);
+    // DCGH
+    texnormvb->setCoord(20, _d); texnormvb->setCoord(21, _c); texnormvb->setCoord(22, _g); texnormvb->setCoord(23, _h);        
+
+    Vec2 uv_a(0,0);
+    Vec2 uv_b(1,0);
+    Vec2 uv_c(1,1);
+    Vec2 uv_d(0,1);
+    Vec2 uv_e(0,0);
+    Vec2 uv_f(1,0);
+    Vec2 uv_g(1,1);
+    Vec2 uv_h(0,1);
+
+    // ABCD 
+    texnormvb->setUV( 0, uv_a ); texnormvb->setUV( 1, uv_b ); texnormvb->setUV( 2, uv_c ); texnormvb->setUV( 3, uv_d );
+    // EFGH
+    texnormvb->setUV( 4, uv_e ); texnormvb->setUV( 5, uv_f ); texnormvb->setUV( 6, uv_g ); texnormvb->setUV( 7, uv_h );
+    // ADHE
+    texnormvb->setUV( 8, uv_a ); texnormvb->setUV( 9, uv_d ); texnormvb->setUV(10, uv_h ); texnormvb->setUV(11, uv_e );
+    // BCGF
+    texnormvb->setUV(12, uv_b ); texnormvb->setUV(13, uv_c ); texnormvb->setUV(14, uv_g ); texnormvb->setUV(15, uv_f );
+    // ABFE
+    texnormvb->setUV(16, uv_a ); texnormvb->setUV(17, uv_b ); texnormvb->setUV(18, uv_f ); texnormvb->setUV(19, uv_e );
+    // DCGH
+    texnormvb->setUV(20, uv_d ); texnormvb->setUV(21, uv_c ); texnormvb->setUV(22, uv_g ); texnormvb->setUV(23, uv_h );
+
+    Vec3 abcd_norm(0,-1,0), efgh_norm(0,1,0), adhe_norm(-1,0,0), bcgf_norm(1,0,0), abfe_norm(0,0,1), dcgh_norm(0,0,-1);
+
+    // ABCD
+    texnormvb->setNormal(0,abcd_norm); texnormvb->setNormal(1,abcd_norm); texnormvb->setNormal(2,abcd_norm); texnormvb->setNormal(3,abcd_norm);
+    // EFGH
+    texnormvb->setNormal(4,efgh_norm); texnormvb->setNormal(5,efgh_norm); texnormvb->setNormal(6,efgh_norm); texnormvb->setNormal(7,efgh_norm);
+    // ADHE
+    texnormvb->setNormal(8,adhe_norm); texnormvb->setNormal(9,adhe_norm); texnormvb->setNormal(10,adhe_norm); texnormvb->setNormal(11,adhe_norm);
+    // BCGF
+    texnormvb->setNormal(12,bcgf_norm); texnormvb->setNormal(13,bcgf_norm); texnormvb->setNormal(14,bcgf_norm); texnormvb->setNormal(15,bcgf_norm);
+    // ABFE
+    texnormvb->setNormal(16,abfe_norm); texnormvb->setNormal(17,abfe_norm); texnormvb->setNormal(18,abfe_norm); texnormvb->setNormal(19,abfe_norm);
+    // DCGH
+    texnormvb->setNormal(20,dcgh_norm); texnormvb->setNormal(21,dcgh_norm); texnormvb->setNormal(22,dcgh_norm); texnormvb->setNormal(23,dcgh_norm);
+    
+
 
 
     g_colmesh = new Mesh();
     g_colmesh->setVertexBuffer(colvb);
-    g_colmesh->setIndexBuffer(cube_ib);
+    g_colmesh->setIndexBuffer(cube_ib_wo_normals);
     g_colmesh->setPrimType( GL_TRIANGLES );
 
     g_texmesh = new Mesh();
     g_texmesh->setVertexBuffer(texvb);
-    g_texmesh->setIndexBuffer(cube_ib);
+    g_texmesh->setIndexBuffer(cube_ib_wo_normals);
     g_texmesh->setPrimType( GL_TRIANGLES );
 
     g_texcolmesh = new Mesh();
     g_texcolmesh->setVertexBuffer(texcolvb);
-    g_texcolmesh->setIndexBuffer(cube_ib);
+    g_texcolmesh->setIndexBuffer(cube_ib_wo_normals);
     g_texcolmesh->setPrimType( GL_TRIANGLES );
+
+    g_texnormmesh = new Mesh();
+    g_texnormmesh->setVertexBuffer(texnormvb);
+    g_texnormmesh->setIndexBuffer(cube_ib_with_normals);
+    g_texnormmesh->setPrimType(GL_TRIANGLES);
 
 
     g_wood_tex = new Texture();
@@ -272,12 +379,13 @@ void setupCube() {
     g_main_layer->insertProp(g_prop_col);
 
 
-    g_prop_tex = new Prop3D();
-    g_prop_tex->setMesh(g_texmesh);
-    g_prop_tex->setScl(Vec3(1,1,1));
-    g_prop_tex->setLoc(Vec3(0,0,0));
-    g_prop_tex->setTexture( g_wood_tex);
-    g_main_layer->insertProp(g_prop_tex);
+    g_prop_texnorm = new Prop3D();
+    g_prop_texnorm->setMesh(g_texnormmesh);
+    g_prop_texnorm->setScl(Vec3(1,1,1));
+    g_prop_texnorm->setLoc(Vec3(0,0,0));
+    g_prop_texnorm->setTexture( g_wood_tex);
+    g_prop_texnorm->setMaterial(g_material);
+    g_main_layer->insertProp(g_prop_texnorm);
 
     g_prop_texcol = new Prop3D();
     g_prop_texcol->setMesh(g_texcolmesh );
@@ -390,6 +498,13 @@ int main() {
     p->setScl(32,32);
     g_hud_layer->insertProp(p);
 
+
+    // light and material
+    g_light = new Light();
+    g_light->pos = Vec3(-50,50,100);
+    g_main_layer->setLight(g_light);    
+    g_material = new Material();
+    g_material->ambient = Color(0.3,0.3,0.3,1);
 
     setupCube();
 
