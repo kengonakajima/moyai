44430c2872804dac790979b332b23232d4218c92
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-04-11 07:52:25 +0900

    cam

--- web/moyai.js
+++ web/moyai.js
@@ -65,21 +65,23 @@ MoyaiClient.prototype.render = function() {
     this.renderer.clearDepth();
     
     // 3d first
+    var camera3d=null; // 最初の3Dレイヤのカメラを採用する。(TODO:レイヤごとに別のカメラで描画できるようにする)
     for(var li=0;li<this.layers.length;li++) {
         var layer = this.layers[li];
         if(layer.viewport.dimension==3) {
             this.render3D(this.scene3d, layer);
-            var camera3d = new THREE.PerspectiveCamera( 45 , this.width / this.height , 0.5 , 2000 );    
-            var lcam = layer.camera;
-            camera3d.up.x = lcam.look_up.x; camera3d.up.y = lcam.look_up.y; camera3d.up.z = lcam.look_up.z;
-            camera3d.position.set( lcam.loc.x, lcam.loc.y, lcam.loc.z );
-            camera3d.lookAt({x: lcam.look_at.x, y: lcam.look_at.y, z: lcam.look_at.z });
-            camera3d.near = layer.viewport.near_clip;
-            camera3d.far = layer.viewport.far_clip;
-            this.renderer.render(this.scene3d, camera3d );            
+            if(camera3d==null) {
+                camera3d = new THREE.PerspectiveCamera( 45 , this.width / this.height , 0.5 , 2000 );    
+                var lcam = layer.camera;
+                camera3d.up.x = lcam.look_up.x; camera3d.up.y = lcam.look_up.y; camera3d.up.z = lcam.look_up.z;
+                camera3d.position.set( lcam.loc.x, lcam.loc.y, lcam.loc.z );
+                camera3d.lookAt({x: lcam.look_at.x, y: lcam.look_at.y, z: lcam.look_at.z });
+                camera3d.near = layer.viewport.near_clip;
+                camera3d.far = layer.viewport.far_clip;
+            }
         }
     }
-
+    this.renderer.render(this.scene3d, camera3d );            
 
 
     // then 2d            
