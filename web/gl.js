6d58dca406e3716fe1aa31451b013c80ee3d441a
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-11-19 09:22:45 +0900

    webgl2

--- web/gl.js
+++ web/gl.js
@@ -4,7 +4,7 @@ var SCRW=1024, SCRH=512;
 function initWebGL(canvas) {
     const opt={antialias:false};
     try {
-        gl = canvas.getContext("webgl",opt) || canvas.getContext("experimental-webgl",opt);
+        gl = canvas.getContext("webgl2",opt);// || canvas.getContext("experimental-webgl",opt);
     } catch(e) {    }
     if(!gl) {
         console.warn("no WebGL support");
@@ -159,7 +159,161 @@ function initNolightShaders() {
     };
 }
 
+function initBigBuffers() {
+    var n=16; //32*32*32=32K cube, x4x6 verts1.5M verts
 
+    const positions = [
+        // Front face
+            -1.0, -1.0,  1.0,
+        1.0, -1.0,  1.0,
+        1.0,  1.0,  1.0,
+            -1.0,  1.0,  1.0,
+
+        // Back face
+            -1.0, -1.0, -1.0,
+            -1.0,  1.0, -1.0,
+        1.0,  1.0, -1.0,
+        1.0, -1.0, -1.0,
+
+        // Top face
+            -1.0,  1.0, -1.0,
+            -1.0,  1.0,  1.0,
+        1.0,  1.0,  1.0,
+        1.0,  1.0, -1.0,
+
+        // Bottom face
+            -1.0, -1.0, -1.0,
+        1.0, -1.0, -1.0,
+        1.0, -1.0,  1.0,
+            -1.0, -1.0,  1.0,
+
+        // Right face
+        1.0, -1.0, -1.0,
+        1.0,  1.0, -1.0,
+        1.0,  1.0,  1.0,
+        1.0, -1.0,  1.0,
+
+        // Left face
+            -1.0, -1.0, -1.0,
+            -1.0, -1.0,  1.0,
+            -1.0,  1.0,  1.0,
+            -1.0,  1.0, -1.0,
+    ];
+
+    const faceColors = [
+        [1.0,  1.0,  1.0,  1.0],    // Front face: white
+        [1.0,  0.0,  0.0,  1.0],    // Back face: red
+        [0.0,  1.0,  0.0,  1.0],    // Top face: green
+        [0.0,  0.0,  1.0,  1.0],    // Bottom face: blue
+        [1.0,  1.0,  0.0,  1.0],    // Right face: yellow
+        [1.0,  0.0,  1.0,  1.0],    // Left face: purple
+    ];
+
+    var colors = [];
+    for (var j = 0; j < faceColors.length; ++j) {
+        const c = faceColors[j];
+        // Repeat each color four times for the four vertices of the face
+        colors = colors.concat(c, c, c, c);
+    }
+
+    const indices = [
+        0,  1,  2,      0,  2,  3,    // front
+        4,  5,  6,      4,  6,  7,    // back
+        8,  9,  10,     8,  10, 11,   // top
+        12, 13, 14,     12, 14, 15,   // bottom
+        16, 17, 18,     16, 18, 19,   // right
+        20, 21, 22,     20, 22, 23,   // left
+    ];
+
+    const textureCoordinates = [
+        // Front
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+        // Back
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+        // Top
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+        // Bottom
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+        // Right
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+        // Left
+        0.0,  0.0,
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  1.0,
+    ];
+    
+
+    var col_ary=new Float32Array(colors.length *n*n*n);
+    var pos_ary=new Float32Array(positions.length *n*n*n);
+    var ind_ary=new Uint32Array(indices.length *n*n*n);
+    var uv_ary=new Float32Array(textureCoordinates.length *n*n*n);
+    
+    for(var y=0;y<n;y++) {
+        for(var x=0;x<n;x++){
+            for(var z=0;z<n;z++) {
+                var ii=z+(x*n)+(y*n*n);
+                var posbi=ii*positions.length;
+                for(var i=0;i<positions.length;i+=3) {
+                    pos_ary[posbi+i+0]=positions[i+0]*0.4+x;
+                    pos_ary[posbi+i+1]=positions[i+1]*0.4+y;
+                    pos_ary[posbi+i+2]=positions[i+2]*0.4+z;
+                }
+                var colbi=ii*colors.length;
+                for(var i=0;i<colors.length;i++) {
+                    col_ary[colbi+i]=colors[i];
+                }
+                var indbi=ii*indices.length;
+                for(var i=0;i<indices.length;i++) {
+                    ind_ary[indbi+i]=indices[i]+24*ii;
+                }
+                var uvbi=ii*textureCoordinates.length;
+                for(var i=0;i<textureCoordinates.length;i++) {
+                    uv_ary[uvbi+i]=textureCoordinates[i];
+                }
+            }
+        }
+    }
+    const positionBuffer=gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER,pos_ary, gl.STATIC_DRAW);
+
+    const colorBuffer=gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER,colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER,col_ary, gl.STATIC_DRAW);
+    
+    const indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, ind_ary, gl.STATIC_DRAW);
+
+    const textureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, uv_ary, gl.STATIC_DRAW);
+
+    console.log("pooo:", pos_ary.length);
+    return {
+        position: positionBuffer,
+        color: colorBuffer,
+        textureCoord: textureCoordBuffer,
+        indices: indexBuffer,
+        vertexCount: 36*n*n*n,        
+    };    
+}
 
 function initBuffers() {
     const positionBuffer=gl.createBuffer();
@@ -327,6 +481,7 @@ function initBuffers() {
         textureCoord: textureCoordBuffer,
         indices: indexBuffer,
         normal: normalBuffer,
+        vertexCount: 36,
     };
 }
 
@@ -340,7 +495,7 @@ function clearScene() {
     gl.enable(gl.BLEND);
     gl.blendFunc(gl.SRC_ALPHA,gl.ONE_MINUS_SRC_ALPHA);
 }
-function drawScene(use_light, projMat, mvMat, programInfo, buf, tex, alpha, xtr,ytr,ztr, xrot,yrot,zrot ) {
+function drawScene(type,vertexCount,use_light, projMat, mvMat, programInfo, buf, tex, alpha, xtr,ytr,ztr, xrot,yrot,zrot ) {
     mat4.identity(mvMat);
     mat4.translate(mvMat, mvMat, [xtr,ytr,ztr]);
     mat4.rotate(mvMat, mvMat, xrot, [1,0,0] );
@@ -401,10 +556,7 @@ function drawScene(use_light, projMat, mvMat, programInfo, buf, tex, alpha, xtr,
 
     // draw!
     {
-        const vertexCount=36;
-        const type=gl.UNSIGNED_SHORT;
-        const offset=0;
-        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+        gl.drawElements(gl.TRIANGLES, vertexCount, type, 0);
 //        const offset=0;
 //        const vertexCount=4;
   //      gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
@@ -478,6 +630,7 @@ function start() {
     var programInfoLight=initLightShaders();
     var programInfoNolight=initNolightShaders();
 
+    const bigbuf=initBigBuffers();
     const buf=initBuffers(use_light);
     const tex = loadTexture("./cubetexture.png");
     var pg_use;
@@ -494,7 +647,7 @@ function start() {
         now*=0.001;
         const dt=now-then;
         then=now;
-        const k=20,d=80;
+        const k=20,d=100;
         clearScene();
         
         gl.useProgram(pg_use.program);
@@ -513,13 +666,17 @@ function start() {
             false,
             projMat
         );        
-        for(var i=0;i<n;i++) {
-            drawScene(use_light, projMat, mvMatArray[i], pg_use,buf,tex, range(0,1), range(-k,k)*2,range(-k,k),range(-d,-d/2), range(1,5),range(1,5),0 );            
+        for(var i=0;i<100;i++) {
+            drawScene(gl.UNSIGNED_INT, bigbuf.vertexCount, use_light, projMat, mvMatArray[i], pg_use, bigbuf,tex, range(0,1), range(-k,k)*2,range(-k,k),range(-d,-d/2), range(1,5),range(1,5),0 );
+            
         }
         var mvMat0=mat4.create();
         var mvMat1=mat4.create();
-        drawScene(use_light, projMat, mvMat0, pg_use,buf,tex, 1.0, Math.sin(g_t)*2,0,-8, g_t,g_t*0.7,0 );
-        drawScene(use_light, projMat, mvMat1, pg_use,buf,tex, 0.2, 0,Math.sin(g_t)*2,-8, g_t*0.7,g_t,0 );        
+        drawScene(gl.UNSIGNED_SHORT,buf.vertexCount,use_light, projMat, mvMat0, pg_use,buf,tex, 1.0, Math.sin(g_t)*2,0,-8, g_t,g_t*0.7,0 );
+        drawScene(gl.UNSIGNED_SHORT,buf.vertexCount,use_light, projMat, mvMat1, pg_use,buf,tex, 0.2, 0,Math.sin(g_t)*2,-8, g_t*0.7,g_t,0 );
+
+
+        
         requestAnimationFrame(render);
         g_t+=1/60;
     }
